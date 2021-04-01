df9bebfd27dceee416533e590f3197f82260a39b
irenemaritsa <irenemaritsa19@gmail.com>
2021-04-01 15:10:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kuispemrogramanmobile/BiodataActivity.java
@@ -0,0 +1,12 @@
+package com.example.kuispemrogramanmobile;
+
+import android.app.Activity;
+import android.view.View;
+
+public class BiodataActivity extends Activity {
+    public void exit(View view) {
+    }
+
+    public void moveMain(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/kuispemrogramanmobile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kuispemrogramanmobile;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void biodata(View view) {
+    }
+
+    public void moveMaps(View view) {
+    }
+
+    public void moveCall(View view) {
+    }
+
+    public void exit(View view) {
+    }
+}
